0393c2042764c3f890487745e90dae9783f8e97c
Breno <bhfs.2908@gmail.com>
2021-10-01 10:47:21 -0300

    feat:estrutura inicial

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import Layout from "../components/template/Layout";
+
 export default function Home() {
   return (
-    <div className={`flex justify-center items-center h-screen bg-gradient-to-r from-green-500 to-blue-500`}>
-      <h1>Admin</h1>
-    </div>
+    <Layout titulo="Pagina Inicial" subtitulo="Estamos construindo um template admin">
+      <h3>Conteudo!!!</h3>
+    </Layout>
   );
 }
