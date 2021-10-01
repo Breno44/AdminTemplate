19bd0dfca5287c6b294fba5fe6785966cf0bdf46
Breno <bhfs.2908@gmail.com>
2021-10-01 13:04:29 -0300

    feat:add componente logo

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Logo from "./Logo";
 import { IconeAjustes, IconeCasa, IconeSino } from "../../icons";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
   return (
     <aside>
+      <div
+        className={`flex flex-col justify-center items-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800`}
+      >
+        <Logo />
+      </div>
       <ul>
         <MenuItem url="/" texto="inicio" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
