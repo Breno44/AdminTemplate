1eaa6d512a8b8ccc6f5c84f3ed4f7ec3e9b2396b
Breno <bhfs.2908@gmail.com>
2021-10-02 10:02:39 -0300

    feat:componente menu lateral 3#

--- src/components/template/MenuLateral.tsx
+++ src/components/template/MenuLateral.tsx
@@ -1,20 +1,28 @@
 import React from "react";
 import Logo from "./Logo";
-import { IconeAjustes, IconeCasa, IconeSino } from "../../icons";
+import { IconeAjustes, IconeCasa, IconeSino, IconeLogout } from "../../icons";
 import MenuItem from "./MenuItem";
 
 export default function MenuLateral() {
   return (
-    <aside>
+    <aside className="flex flex-col">
       <div
         className={`flex flex-col justify-center items-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800`}
       >
         <Logo />
       </div>
-      <ul>
+      <ul className="flex-grow">
         <MenuItem url="/" texto="inicio" icone={IconeCasa} />
         <MenuItem url="/ajustes" texto="Ajustes" icone={IconeAjustes} />
         <MenuItem url="/notificacoes" texto="Notificações" icone={IconeSino} />
+      </ul>
+      <ul>
+        <MenuItem
+          texto="Sair"
+          icone={IconeLogout}
+          onClick={() => alert("você saiu")}
+          className={`text-red-600 hover:bg-red-400 hover:text-white`}
+        />
       </ul>
     </aside>
   );
